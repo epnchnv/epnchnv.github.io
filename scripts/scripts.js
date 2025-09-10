b876a8cbd1f67e81ffe034eb871cd55451794b19
epnchnv <pelirrojabruja1@gmail.com>
2025-09-10 13:12:40 +0300

    поправила боки

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,51 +1,51 @@
 var images = [
-  ["robin/1.jpeg",
-  "robin/2.jpeg",
-  "robin/3.jpeg",
-  "robin/4.jpeg",
-  "robin/5.jpeg",
-  "robin/6.jpeg",
-  "robin/7.jpeg",
-  "robin/8.jpeg",
-  "robin/9.jpeg",
-  "robin/10.jpeg",
-  "robin/11.jpeg",
-  "robin/12.jpeg",
-  "robin/13.jpeg",
-  "robin/14.jpeg",
-  "robin/15.jpeg",
-  "robin/16.jpeg",
-  "robin/17.jpeg"],
+  ["robin/1.jpeg","robin/2.jpeg","robin/3.jpeg","robin/4.jpeg","robin/5.jpeg",
+   "robin/6.jpeg","robin/7.jpeg","robin/8.jpeg","robin/9.jpeg","robin/10.jpeg",
+   "robin/11.jpeg","robin/12.jpeg","robin/13.jpeg","robin/14.jpeg","robin/15.jpeg",
+   "robin/16.jpeg","robin/17.jpeg"],
   ["Сонная булочка, но не хочешь упускать момент",
-    "После окончания лета, сразу ждешь новый год",
-    "Ты лучший подарочек для всех",
-    "Тебе лучше всего подойдет фраза: Поспали - можно и поесть, поели- можно и поспать",
-    "Ты любишь учится и самосовершенствоваться! Везде тебя ждет упех!",
-    "Любишь свое день рождение, подарки и поесть",
-    "Путешетвия - твоя страсть! Ты всегда знаешь, чем занять себя в дороге",
-    "Радуешься походам в рестораны",
-    "Мечтаешь о отдыхе на море",
-    "Очень сильно устал спасать этот мир от грусти и печали",
-    "Тебе не хватает фотосессии в поле с цветами",
-    "Сближаешь других людей",
-    "Посещаешь лучшие бутики",
-    "Любишь валяться с близкими",
-    "Ты рад активному отдыху",
-    "Любишь получать цветы",
-    "Экстроверт - твое второе имя"
+   "После окончания лета, сразу ждешь новый год",
+   "Ты лучший подарочек для всех",
+   "Тебе лучше всего подойдет фраза: Поспали - можно и поесть, поели- можно и поспать",
+   "Ты любишь учится и самосовершенствоваться! Везде тебя ждет упех!",
+   "Любишь свое день рождение, подарки и поесть",
+   "Путешетвия - твоя страсть! Ты всегда знаешь, чем занять себя в дороге",
+   "Радуешься походам в рестораны",
+   "Мечтаешь о отдыхе на море",
+   "Очень сильно устал спасать этот мир от грусти и печали",
+   "Тебе не хватает фотосессии в поле с цветами",
+   "Сближаешь других людей",
+   "Посещаешь лучшие бутики",
+   "Любишь валяться с близкими",
+   "Ты рад активному отдыху",
+   "Любишь получать цветы",
+   "Экстроверт - твое второе имя"
   ]
-
 ];
 
-
+// Показ картинки и подписи (лотерея)
 document.getElementById("my_button").addEventListener("click", function() {
-   var i = Math.floor(Math.random()*17)%17;
-  document.getElementById("my_image").src = images[0][i];
-  document.getElementById("my_image").className="photo-robin";
-  document.getElementById("figcaption").innerHTML =images[1][i];
-  document.getElementById("figcaption").className="caption"
-})
-// --- Новый код для бургер-меню ---
+  var i = Math.floor(Math.random() * 17) % 17;
+
+  const image = document.getElementById("my_image");
+  const caption = document.getElementById("figcaption");
+
+  // Сброс активных состояний
+  image.classList.remove("active");
+  caption.classList.remove("active");
+
+  // Меняем картинку и подпись
+  image.src = images[0][i];
+  caption.innerHTML = images[1][i];
+
+  // Немного задерживаем для анимации
+  setTimeout(() => {
+    image.classList.add("active");
+    caption.classList.add("active");
+  }, 50);
+});
+
+// Бургер-меню
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
 
